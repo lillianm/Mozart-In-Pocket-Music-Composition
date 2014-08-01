8036513957344e507677e92e7e7d2d7748334bcd
YiMa <yima9102@gmail.com>
2014-07-31 18:17:56 -0700

    Mozart in pocket , source code

---
+++ gen/com/androidApp/mozartinpocket/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.androidApp.mozartinpocket;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/androidApp/mozartinpocket/musiclist/EmptyFragment.java
@@ -0,0 +1,23 @@
+package com.androidApp.mozartinpocket.musiclist;
+
+import com.androidApp.mozartinpocket.LoginActivity;
+
+import com.androidApp.mozartinpocket.R;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class EmptyFragment extends Fragment{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState){
+		View rootView = inflater.inflate(R.layout.fragment_empty, container,false);
+		TextView text = (TextView) rootView.findViewById(R.id.txtView_empty_message);
+		text.setText("  Hello!    " +LoginActivity.currentUser.getUsername() + "!\n" + "  You do not have any music files yet\n  Use Compose Function\n to compose your OWN music!");
+		return rootView;
+	}
+}
